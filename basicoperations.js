3445c08e9555d005ddb567920b1b00b4b6eefcef
Manikanta Jami <Manikanta_Jami@epam.com>
2017-07-25 15:51:49 +0530

    update filenames

--- basicoperations.js
+++
@@ -1,5 +0,0 @@
-function basicOperations(operation, value1, value2) {
-
-    var result = value1 + operation + value2;
-    return eval(result);
-}
--- opposite.js
+++
@@ -1,5 +0,0 @@
-function opposite(number) {
-
-    return number * -1
-
-}
