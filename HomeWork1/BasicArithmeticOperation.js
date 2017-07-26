0f2668aa2211e9eea3f8b09627e6885faccad465
Manikanta Jami <Manikanta_Jami@epam.com>
2017-07-26 17:32:55 +0530

    added switch case to basic operation

--- HomeWork1/BasicArithmeticOperation.js
+++ HomeWork1/BasicArithmeticOperation.js
@@ -1,4 +1,12 @@
-function basicOp(operation, value1, value2){
-   var result = value1 + operation + value2;
-    return eval(result);
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+': return value1 + value2;
+            break;
+        case '-': return value1 - value2;
+            break;
+        case '*': return value1 * value2;
+            break;
+        case '/': return value1 / value2;
+            break;
+    }
 }
