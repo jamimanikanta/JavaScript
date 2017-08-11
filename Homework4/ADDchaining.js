e3254848d46f9b3af2697722e272230b6f53bd5b
jamimanikanta <jamimanikanta369@gmail.com>
2017-08-11 18:45:05 +0530

    Update ADDchaining.js

--- Homework4/ADDchaining.js
+++ Homework4/ADDchaining.js
@@ -1,19 +1,5 @@
-var add = (function() {
-debugger;
-    var factory = function(value) {
-    debugger;
-        var fn = function(num) {
-        debugger;
-            return factory(value + num);
-        };
-        // This is the main hack: 
-        // We will return a function that when compared / concatted will call .toString and return a number.
-        // Never use this in production code...
-        fn.toString = function() {
-        debugger;
-            return value;
-        };
-        return fn;
-    };
-    return factory(0);
-})();
+function add(x) {
+  function fn(n) { return add(x + n) };
+  fn.valueOf = function() { return x };
+  return fn;
+}
