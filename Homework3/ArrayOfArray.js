9217b9a070b216f84440b8e11430882aee2319d9
jamimanikanta <jamimanikanta369@gmail.com>
2017-08-11 14:18:24 +0530

    Update ArrayOfArray.js

--- Homework3/ArrayOfArray.js
+++ Homework3/ArrayOfArray.js
@@ -1,8 +1,9 @@
 function prefill(n, v) {
-debugger;
-  if (typeof n === 'boolean' || ~~n != n || +n < 0) {throw new TypeError(n + ' is invalid')}
-  
-  return Array.apply(null, new Array(parseInt(n, 10))).map(function() {
-  debugger;
-  return v;});
+    if (typeof n === 'boolean' || ~~n != n || +n < 0) {
+
+        throw new TypeError(n + ' is invalid')
+    }
+    return Array.apply(null, new Array(parseInt(n, 10))).map(function () {
+        return v;
+    });
 }
