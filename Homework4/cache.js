bd0838fae2349b7c260cccb6fd77ad65c1b40d67
jamimanikanta <jamimanikanta369@gmail.com>
2017-09-12 20:28:00 +0530

    Update cache.js

--- Homework4/cache.js
+++ Homework4/cache.js
@@ -1,8 +1,20 @@
 function cache(func) {
-  var result = {};
+  // do your magic here
+  var arr = [];
+  //check the arguments
   return function() {
-    var args = JSON.stringify(arguments);
-    if (!result.hasOwnProperty(args)) result[args] = func.apply(null, arguments);
-    return result[args];
-  };
+     var funcArgs = arguments;
+    for (var i = 0, length = arr.length; i < length; i++) {
+      var args = arr[i].args;
+      if(args.length !== arguments.length) continue;
+      if (Array.prototype.slice.call(args).every(function(val, idx) {
+          return  val === funcArgs[idx];
+        })) {
+        return arr[i].result;
+      }
+    }
+    var result =  func.apply(null, arguments);
+    arr.push({ "args" :arguments , "result" :  result});
+    return result;
+  }
 }
